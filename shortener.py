8ae46da87bf95bef1c06edef38985cf1bbc9c5b3
hrzhu <a@hrzhu.me>
2013-01-20 00:47:04 +0800

    implement url shorten and url redirect

--- shortener.py
+++ shortener.py
@@ -3,7 +3,9 @@
 import random
 import re
 from string import digits, letters
-from flask import Flask
+from flask import Flask, redirect, flash, url_for, render_template, request, \
+    session, abort
+from werkzeug.routing import BaseConverter
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -19,21 +21,60 @@ def url_to_id(url):
         return BASE.find(url[0]) * 62 * 62 + BASE.find(url[1]) * 62 \
             + BASE.find(url[2])
 
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
 app = Flask('Shortner')
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/shortener.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shortener.db'
+app.secret_key = 'DEV'
+app.url_map.converters['regex'] = RegexConverter
 db = SQLAlchemy(app)
 
 
 class Url(db.Model):
     __tablename__ = 'Urls'
-    org = db.Column(db.String, nullable=False)
-    short = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String, nullable=False)
+    short_url_id = db.Column(db.Integer, nullable=False,
+                             primary_key=True, autoincrement=False)
 
-    def __init__(self, org, short):
-        self.org = org
-        self.short = short
+    def __init__(self, long_url, short_url_id):
+        self.long_url = long_url
+        self.short_url_id = short_url_id
 
+
+@app.route("/")
+def shorten():
+    url = None
+    if session.get('url'):
+        url = session['url']
+        session.pop('url', None)
+    return render_template("shortener.html", url=url)
+
+
+@app.route('/<regex("[\w]{3}$"):url>')
+def redirect_url(url):
+    long_url = Url.query.get(url_to_id(url))
+    if long_url:
+        return redirect(long_url.long_url)
+    else:
+        return abort(404)
+
+@app.route("/shorten", methods=['POST'])
+def shorten_url():
+    if request.form['text']:
+        long_url = request.form['text']
+        short_url = gen_url()
+        session['url'] = short_url
+        short_url_id = url_to_id(short_url)
+        url = Url(long_url, short_url_id)
+        db.session.add(url)
+        db.session.commit()
+        flash("Url is sucessfully shortened to")
+    return redirect(url_for('shorten'))
 
 if __name__ == '__main__':
     db.create_all(app=app)
